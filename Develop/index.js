92a6a3afa5fbcfd93637dc45837d6a2dd129c3b3
BEhnert <behnert77@gmail.com>
2020-09-26 00:32:23 -0500

    90% complete, need license/badge

--- Develop/index.js
+++ Develop/index.js
@@ -1,33 +1,72 @@
-// array of questions for user
-const questions = [
+// // array of questions for user
+// const questions = [
 
-];
+// ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+//like generateHtml from 40
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
 
-}
+// // function to initialize program
+// function init() {
 
-// function call to initialize program
-init();
+// }
 
-var inquirer = require("inquirer");
-var fs = require('fs');
+//like promptUser from 40
+// // function call to initialize program
+// init();
 
-inquirer.prompt([
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+const mdGen = require('./utils/generateMarkdown')
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser(){
+return inquirer.prompt([
   {
     type: "input",
-    name: "name",
-    message: "What is your name?"
+    name: "title",
+    message: "What is the title of project?",
+    default: "ReadMe Test"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a description of the project.",
+    default: "Good vibrations"
+  }, 
+  {
+    type: "input",
+    name: "instructions",
+    message: "What are the installation instructions??",
+    default: "Push Play"
+  }, 
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage of the project?",
+    default: "Fun"
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "Were there any notable outside contributions for this project?",
+    default: "God"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How is this project tested?",
+    default: "Carefully"
   },
   {
     type: "checkbox",
-    message: "What languages do you know?",
-    name: "stack",
+    message: "Choose a license for the project.",
+    name: "license",
     choices: [
       "HTML", 
       "CSS", 
@@ -36,26 +75,59 @@ inquirer.prompt([
     ]
   },
   {
-    type: "list",
-    message: "What is your preferred method of communication?",
-    name: "contact",
-    choices: [
-      "email",
-      "phone",
-      "telekinesis"
-    ]
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+    default: "BEhnert"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+    default: "behnert77@gmail.com"
   }
-]).then(function(data) {
+])};
 
-  var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+//   {
+//     type: "list",
+//     message: "What is your preferred method of communication?",
+//     name: "contact",
+//     choices: [
+//       "email",
+//       "phone",
+//       "telekinesis"
+// //     ]
+// ]).then(function(data) {
 
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+//   var filename = data.title.toUpperCase().split(' ').join('') + ".json";
 
-    if (err) {
-      return console.log(err);
+//   fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+//     if (err) {
+//       return console.log(err);
+//     }
+
+//     console.log("Success!");
+
+//   });
+// });
+
+
+async function init() {
+    console.log("hi")
+    try {
+      const answers = await promptUser();
+  
+      var filename = answers.title.toUpperCase().split(' ').join('') + ".md";
+      const readme = mdGen(answers);
+  
+      await writeFileAsync(filename, readme);
+  
+      console.log("Successfully wrote a new README Markdown file");
+    } catch(err) {
+      console.log(err);
     }
-
-    console.log("Success!");
-
-  });
-});
+  }
+  
+  init();
+  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,46 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+  ##### Table of Contents
 
+   [Title](#title)
+   [Description](#description)
+   [Installation](#installation)
+   [Usage](#usage)
+   [Contributing](#contributing)
+   [Tests](#tests)
+   [Questions](#questions)
+   [License](#license)
+
+  # Title
+  # ${answers.title}
+
+  ### Description
+  ${answers.description}
+
+  ### Instructions
+  ${answers.instructions}
+
+  ### Usage
+  ${answers.usage}
+
+  ### Contributing
+  ${answers.contributing}
+
+  ### Tests
+  ${answers.tests}
+
+  ### Questions
+       GitHub - <add link here ${answers.github}
+
+       For additional questions, please email me at: ${answers.email}
+
+  #### License
+
+    ${answers.license}
+    
+  Good Test of ReadMe file
+  Will this write again?
 `;
 }
 
